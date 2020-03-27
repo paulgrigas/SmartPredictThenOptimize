e45f1f19479936efe7b6f845bc7f8de6ce0162ac
paulgrigas <paulgrigas@gmail.com>
2020-03-27 01:37:31 -0700

    Create noreg_expt.sh

---
+++ experiments/noreg_expt.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=spo_shortestpath_noreg_march2020
+#
+# Partition:
+#SBATCH --partition=savio2
+#
+# Request one node:
+#SBATCH --nodes=1
+#
+# Specify number of tasks for use case:
+#SBATCH --ntasks-per-node=24
+#
+# Processors per task:
+#SBATCH --cpus-per-task=1
+#
+# Wall clock limit:
+#SBATCH --time=20:00:00
+#
+# Mail type:
+#SBATCH --mail-type=all
+#
+# Mail user:
+#SBATCH --mail-user=pgrigas@berkeley.edu
+#
+## Command(s) to run:
+julia shortest_path_run_noreg.jl
